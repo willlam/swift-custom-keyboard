61896eb4a19662e2a0afe32c7afa0fa0a52fbf8f
will <will@willlam.net>
2015-08-25 13:39:06 -0400

    added text to proxy.insertText

---
+++ kustomkeyboard/KeyboardViewController.swift
@@ -0,0 +1,86 @@
+//
+//  KeyboardViewController.swift
+//  kustomkeyboard
+//
+//  Created by William Lam on 2015-08-24.
+//  Copyright (c) 2015 William Lam. All rights reserved.
+//
+
+import UIKit
+
+class KeyboardViewController: UIInputViewController {
+
+    @IBOutlet var nextKeyboardButton: UIButton!
+
+    override func updateViewConstraints() {
+        super.updateViewConstraints()
+    
+        // Add custom view sizing constraints here
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+      
+      // Add a customized button
+      let button = UIButton.buttonWithType(.System) as! UIButton
+      button.frame = CGRectMake(150, 40, 100, 120)
+      
+      var cat = UIImage(named: "cat2.jpg")
+      button.setBackgroundImage(cat, forState: .Normal)
+      
+      button.addTarget(self, action: "buttonPressed", forControlEvents: .TouchUpInside)
+      
+      self.view.addSubview(button)
+    
+        // Perform custom UI setup here
+        self.nextKeyboardButton = UIButton.buttonWithType(.System) as! UIButton
+    
+        self.nextKeyboardButton.setTitle(NSLocalizedString("Next Keyboard", comment: "Title for 'Next Keyboard' button"), forState: .Normal)
+        self.nextKeyboardButton.sizeToFit()
+        self.nextKeyboardButton.setTranslatesAutoresizingMaskIntoConstraints(false)
+    
+        self.nextKeyboardButton.addTarget(self, action: "advanceToNextInputMode", forControlEvents: .TouchUpInside)
+        
+        self.view.addSubview(self.nextKeyboardButton)
+    
+        var nextKeyboardButtonLeftSideConstraint = NSLayoutConstraint(item: self.nextKeyboardButton, attribute: .Left, relatedBy: .Equal, toItem: self.view, attribute: .Left, multiplier: 1.0, constant: 0.0)
+        var nextKeyboardButtonBottomConstraint = NSLayoutConstraint(item: self.nextKeyboardButton, attribute: .Bottom, relatedBy: .Equal, toItem: self.view, attribute: .Bottom, multiplier: 1.0, constant: 0.0)
+        self.view.addConstraints([nextKeyboardButtonLeftSideConstraint, nextKeyboardButtonBottomConstraint])
+      
+      
+    }
+  
+  
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated
+    }
+
+    override func textWillChange(textInput: UITextInput) {
+        // The app is about to change the document's contents. Perform any preparation here.
+    }
+
+    override func textDidChange(textInput: UITextInput) {
+        // The app has just changed the document's contents, the document context has been updated.
+    
+        var textColor: UIColor
+        var proxy = self.textDocumentProxy as! UITextDocumentProxy
+        if proxy.keyboardAppearance == UIKeyboardAppearance.Dark {
+            textColor = UIColor.whiteColor()
+        } else {
+            textColor = UIColor.blackColor()
+        }
+        self.nextKeyboardButton.setTitleColor(textColor, forState: .Normal)
+    }
+  
+  func buttonPressed() {
+    // A proxy to the text input object that the custom keyboard is interacting with. (read-only)
+    var proxy = textDocumentProxy as! UITextDocumentProxy
+    
+    // Insert a character into the displayed text.
+    proxy.insertText("¯\\_(ツ)_/¯")
+  }
+  
+
+}
